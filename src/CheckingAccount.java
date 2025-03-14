4789184b84bbd94aac305ac801ef23286090e628
organ-elle <eea2250538@slu.edu.ph>
2025-03-14 19:28:58 +0800

    sabi kasi withdrawal not allowed pero idk

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class CheckingAccount extends BankAccounts {
     private double minimumBalance;
 
@@ -16,8 +18,11 @@ public class CheckingAccount extends BankAccounts {
         return minimumBalance;
     }
 
-    // Encash check method (withdraw equivalent)
+    // Encash check method (withdraw and encash are not synonymous)
+    // machines are not used to validate encashed checks and the amount is not withdrawn from the account
     public void encashCheck(double amount) {
-        withdraw(amount);
+        //withdraw(amount);
+        System.out.println("Please see a teller to encash check of " +amount+ ". You may also deposit the check to an account but cash withdrawal after deposit is not allowed.");
     }
+
 }
