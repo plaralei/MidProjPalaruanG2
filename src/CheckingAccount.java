1add757f816ebeecb185f5e3ab7a7d279e801502
organ-elle <eea2250538@slu.edu.ph>
2025-03-18 20:38:57 +0800

    unfinished javadocs, added getMinimumBalance

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -2,9 +2,25 @@
 2 members
  */
 
+/**
+ * The CheckingAccount class holds the methods to get the account's minimum balance or to encash a check
+ *This class allows the user the options on what to do with a checking account
+ *
+ * @bsok/organ-elle
+ * @version 1.0
+ */
+
 public class CheckingAccount extends BankAccounts {
     public double minimumBalance;
-    
+
+    /**
+     * Checks the minimum balance of the account
+     *
+     * @param accountNo Ensures that the account number aligns with that of a created account
+     * @param accountName Ensures that the account name aligns with that of a created account
+     * @param minimumBalance Ensures that the account number aligns with that of a created account
+     *
+     */
     public CheckingAccount(int accountNo, String accountName, double minimumBalance) {
         super(accountNo, accountName);
         if (minimumBalance < 0) {
@@ -12,7 +28,17 @@ public class CheckingAccount extends BankAccounts {
         }
         this.minimumBalance = minimumBalance;
     }
-    
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    /**
+     *
+     *
+     * @param amount The amount on the check
+     * @throws InsufficientBalanceException if the balance falls below the minimum balance
+     */
     public void encashCheck(double amount) throws InsufficientBalanceException {
         if (inquireBalance() - amount < minimumBalance) {
             throw new InsufficientBalanceException("Cannot encash check: Balance would fall below minimum requirement");
